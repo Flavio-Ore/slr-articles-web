c819ec1e30266daaf5be98eb127f341c76c2503c
Flavio Ore <101116042+Flavio-Ore@users.noreply.github.com>
2026-09-17 12:58:00 -0500

    config: compiler, removeConsole.logs, remove unnecesary props in the prompt

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     viewTransition: true
+  },
+  compiler: {
+    removeConsole: { exclude: ['error'] }
   }
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/(slr)/services/slr-analyzer.ts
+++ src/app/(slr)/services/slr-analyzer.ts
@@ -5,13 +5,14 @@ import { createPartFromUri, type Part } from '@google/genai'
 
 const PROMPT: (string | Part)[] = [
   `Analyze the following scientific papers. For each paper, extract the following information:
-      - fileName: The display name of the file.
       - title: The full title of the paper.
       - url: The source URL of the paper.
       - source: The journal or conference where it was published.
       - year: The publication year.
       - countries: An array of countries of the authors' affiliations.
       - issn: The ISSN of the journal.
+      - publicationType: The type of publication (e.g., 'Scientific Article', 'Review Paper').
+      - numberOfCitations: The total number of citations the paper has received.
       - authors: An array of objects, each with the author's 'name', 'affiliation', and 'hIndex' (if available, otherwise null).
       - quartiles: The journal's quartile (e.g., 'Q1', 'Q2').
       - resume: A brief summary or abstract of the paper.
